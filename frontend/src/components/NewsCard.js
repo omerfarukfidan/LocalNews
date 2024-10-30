90acde113fd5f8b0b5303d01dee6c4b60094c651
Ömer Faruk Fidan <fidanomerfaruk01@gmail.com>
2024-10-30 16:12:48 +0100

    provider defined

--- frontend/src/components/NewsCard.js
+++ frontend/src/components/NewsCard.js
@@ -1,8 +1,32 @@
-import React from 'react';
-import '../styles/NewsCard.css';
+import React, { useEffect, useState } from 'react';
 import { fetchNewsUrl } from '../services/api';
+import '../styles/NewsCard.css';
 
 function NewsCard({ title, content, NewsID }) {
+  const [provider, setProvider] = useState('Unknown Provider');
+
+  useEffect(() => {
+    const getProvider = async () => {
+      const newsData = await fetchNewsUrl(NewsID);
+      if (newsData.url) {
+        const url = newsData.url;
+
+        // Determine the provider based on the URL prefix
+        if (url.substring(0, 15) === 'https://edition') {
+          setProvider('CNN News');
+        } else if (url.substring(0, 15) === 'https://eu.usat') {
+          setProvider('USA Today News');
+        } else {
+          setProvider('Unknown Provider');
+        }
+      } else {
+        setProvider('Unknown Provider');
+      }
+    };
+
+    getProvider();
+  }, [NewsID]);
+
   const handleReadMore = async () => {
     const newsData = await fetchNewsUrl(NewsID);
     if (newsData.url) {
@@ -16,6 +40,7 @@ function NewsCard({ title, content, NewsID }) {
     <div className="news-card">
       <h2>{title}</h2>
       <p>{content.substring(0, 150)}...</p>
+      <span className="news-provider">{provider}</span>
       <button onClick={handleReadMore} className="read-more-button">Read More</button>
     </div>
   );
